f962a24a937b138f4cb0a6016f893d6f23b79a88
Wanja Zaeske <wanja.zaeske@dlr.de>
2022-10-17 16:23:09 +0200

    package vitis and vivado

--- flake.nix
+++ flake.nix
@@ -48,62 +48,84 @@
         genProductList = products: builtins.concatStringsSep "," (
           pkgs.lib.mapAttrsToList (name: value: if value then "${name}:1" else "${name}:0")
             products);
-        build-xilinx-toolchain = { name ? "Vivado", src, edition, products, version }:
+        build-xilinx-toolchain = { name, src, edition, products, version }:
           let
             nameLowercase = pkgs.lib.toLower name;
             fhsName = nameLowercase + "-fhs";
             toolchain-raw = pkgs.stdenv.mkDerivation rec {
               inherit src version;
               pname = nameLowercase + "-raw";
-              nativeBuildInputs = with pkgs; [ pigz xorg.xorgserver (genFhs { }) ];
-              unpackCmd = "tar -I pigz -xf $curSrc";
-              dontFixup = true;
-
+              nativeBuildInputs = with pkgs; [ xorg.xorgserver (genFhs { }) ];
+              dontStrip = true;
+              dontPatchELF = true;
+              noAuditTmpdir = true;
+              dontPruneLibtoolFiles = true;
               installPhase = ''
                 runHook preInstall
 
                 substitute ${./assets/install_config.txt} install_config.txt \
-                  --subst-var out \
-                  --subst-var-by products '${genProductList products}' \
-                  --subst-var-by edition '${edition}'
+                  --subst-var-by edition '${edition}' --subst-var out \
+                  --subst-var-by products '${genProductList products}'
 
                 export DISPLAY=:1
                 Xvfb $DISPLAY &
                 xvfb_pid=$!
-                xilinx-fhs xsetup --agree XilinxEULA,3rdPartyEULA,WebTalkTerms --batch Install --config install_config.txt
+                xilinx-fhs xsetup --agree XilinxEULA,3rdPartyEULA,WebTalkTerms \
+                  --batch Install --config install_config.txt
                 kill $xvfb_pid
-
+ 
                 runHook postInstall
               '';
             };
-            fhs = genFhs {
-              name = fhsName;
-              profile = ''
-                export LC_NUMERIC="en_US.UTF-8"
-                source ${toolchain-raw}/${name}/*/settings64.sh
-              '';
-            };
+            wrapper = pkgs.runCommand (nameLowercase + "-wrapped") { nativeBuildInputs = [ pkgs.makeWrapper ]; } ''
+              mkdir -p $out/bin
+              for dir in $(find ${toolchain-raw}/ -maxdepth 3 -type f -name 'settings64.sh' -exec dirname {} \;)
+              do
+                for file in $(find "$dir" -maxdepth 2 -path '*/bin/*' -type f -executable)
+                do
+                  makeWrapper \
+                      "${genFhs { runScript = ""; }}/bin/xilinx-fhs" \
+                      "$out/bin/''${file##*/}" \
+                      --run "$dir/settings64.sh" \
+                      --set LC_NUMERIC 'en_US.UTF-8' \
+                      --add-flags "\"$file\""
+                done
+              done
+            '';
           in
-          pkgs.symlinkJoin { name = "${nameLowercase}-${edition}"; paths = [ toolchain-raw fhs ]; };
+          wrapper;
+        # pkgs.symlinkJoin { name = "${nameLowercase}-${edition}"; paths = [ toolchain-raw fhs ]; };
       in
       {
-        packages = builtins.listToAttrs (pkgs.lib.flatten (pkgs.lib.mapAttrsToList
-          (version: { editions, products, sha256 }: builtins.map
-            (edition: pkgs.lib.nameValuePair
-              (pkgs.lib.toLower (
-                builtins.replaceStrings [ " " "." ] [ "-" "-" ] "${edition}-${version}"
-              ))
-              (build-xilinx-toolchain {
-                name = "Vivado";
-                src = pkgs.requireFile {
-                  name = "Xilinx_Vivado_${version}.tar.gz";
-                  url = "https://www.xilinx.com/";
-                  inherit sha256;
-                };
-                inherit edition products version;
-              }))
-            editions)
-          versions));
+        packages = builtins.listToAttrs
+          (pkgs.lib.flatten (pkgs.lib.mapAttrsToList
+            (version: { editions, products, sha256 }: builtins.map
+              (edition: pkgs.lib.nameValuePair
+                (pkgs.lib.toLower (
+                  builtins.replaceStrings [ " " "." ] [ "-" "-" ] "${edition}-${version}"
+                ))
+                (build-xilinx-toolchain {
+                  name = builtins.elemAt (pkgs.lib.splitString "_" version) 0;
+                  src = pkgs.requireFile {
+                    name = "Xilinx_${version}.tar.gz";
+                    url = "https://www.xilinx.com/";
+                    inherit sha256;
+                  };
+                  inherit edition products version;
+                })
+              )
+              editions
+            )
+            versions)) //
+        {
+          fhs-test = genFhs { runScript = ""; };
+          makeWrapper-test = pkgs.runCommand "mwt" { nativeBuildInputs = [ pkgs.makeWrapper ]; } ''
+            mkdir -p $out/bin
+            touch $out/bin/dada
+            chmod +x $out/bin/dada
+            makeWrapper $out/bin/dada $out/bin/dada-wrapped --add-flags vitis --set XYZ ABC
+          '';
+        };
       }
     );
 }
